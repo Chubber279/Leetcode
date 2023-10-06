034df3352156442ed4dfdec8be7e7ad8a82ae2de
Chubber279 <lorenzo.ocampo279@gmail.com>
2023-10-06 13:20:56 +0800

    Implemented iterative solution. Use two pointers to iterate over the linked lists. If a pointer reaches the end, point it to the start of the other linked list. The meeting point will be their intersection point.

---
+++ Easy/Intersection_of_two_linked_lists.py
@@ -0,0 +1,15 @@
+from typing import *
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        pointerA = headA
+        pointerB = headB
+        while pointerA !=  pointerB:
+            pointerA = headB if pointerA is None else pointerA.next
+            pointerB = headA if pointerB is None else pointerB.next
+        return pointerA
