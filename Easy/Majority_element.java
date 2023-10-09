a966a869e5b2a0925a44bfa30eb0b62e94129a43
Chubber279 <lorenzo.ocampo279@gmail.com>
2023-10-09 15:15:31 +0800

    Solved using Moore's Voting Algorithm. This algorithm uses a count and a candidate variable to identify the majority element. If count is 0, assign current number to as candidate and increment count. Else, check current number if equal to candidate. If equal, increment count, else decrement. The remaining candidate is the majority element

---
+++ Easy/Majority_element.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+                count++;
+            } else {
+                if (candidate == num) {
+                    count++;
+                } else {
+                    count--;
+                }
+            }
+        }
+        return candidate;
+    }
+}
